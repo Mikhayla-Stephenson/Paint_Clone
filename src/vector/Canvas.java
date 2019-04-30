139c287eaca96b74e8a9ff99e8729b3f457077d0
Joshua Inglis <jt.inglis@outlook.com>
2019-04-30 15:11:39 +1000

    Added Canvas Class

--- src/vector/Canvas.java
+++ src/vector/Canvas.java
@@ -1,4 +1,37 @@
 package vector;
 
+import java.awt.*;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Canvas {
+    private List<VectorShape> shapes;
+    private Type selectedTool;
+    private Color currentPenColor;
+    private Color currentFillColor;
+    private boolean leftMouse;
+    private Coord mouseLocation;
+
+
+    public Canvas() {
+        shapes = new LinkedList<>();
+        selectedTool = Type.LINE;
+    }
+
+    public VectorShape addShape(Type type, Point startingPoint) {
+        VectorShape shape = new VectorShape(type, startingPoint, currentPenColor, currentFillColor);
+        shapes.add(shape);
+        return shape;
+    }
+
+    public void drag(VectorShape shape, int point) {
+        while (leftMouse) {
+            shape.editPoint(point, mouseLocation);
+            try {
+                Thread.sleep((long) 60);
+            } catch (InterruptedException error) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
 }
